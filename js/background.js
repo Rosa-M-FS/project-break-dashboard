05a1eec561b5d13377b7f8bc85775f698104581b
Rosa-M-FS <rmlopezrobles@gmail.com>
2024-12-16 01:09:32 +0100

    Ejercicio Terminado para pages

--- js/background.js
+++ js/background.js
@@ -1,9 +1,26 @@
-
+let basePath = "./";
+if (window.location.pathname.includes("/subcarpeta/")) {
+    basePath = "../";
+}
 const img = [
-    "../img/img1.jpg","../img/img2.jpg","../img/img3.jpg","../img/img4.jpg","../img/img5.jpg","../img/img6.jpg",
-    "../img/img7.jpg","../img/img8.jpg","../img/img9.jpg","../img/img10.jpg","../img/img11.jpg",
+    `${basePath}img/img1.jpg`,
+    `${basePath}img/img2.jpg`,
+    `${basePath}img/img3.jpg`,
+    `${basePath}img/img4.jpg`,
+    `${basePath}img/img5.jpg`,
+    `${basePath}img/img6.jpg`,
+    `${basePath}img/img7.jpg`,
+    `${basePath}img/img8.jpg`,
+    `${basePath}img/img9.jpg`,
+    `${basePath}img/img10.jpg`,
+    `${basePath}img/img11.jpg`
 ];
 
+/* const img = [
+    "./img/img1.jpg","./img/img2.jpg","./img/img3.jpg","./img/img4.jpg","./img/img5.jpg","./img/img6.jpg",
+    "./img/img7.jpg","./img/img8.jpg","./img/img9.jpg","./img/img10.jpg","./img/img11.jpg",
+];
+ */
 let imgNum = 0;
 
 // Función para cambiar la imagen de fondo
